628e8f95f5a7d3ebfa239d9471c978cd26260e77
Gio4E20 <37852106+Gio4E20@users.noreply.github.com>
2020-12-16 01:27:32 -0300

    Add .gitignore

---
+++ src/main/java/FreelanceAssister.java
@@ -0,0 +1,14 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class FreelanceAssister {
+
+    public static void main(String args[]) throws Exception {
+
+        JDA jda = new JDABuilder("").build();
+
+    }
+
+}
