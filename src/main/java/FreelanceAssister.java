1151dfbcfd9b32833f9a9f3c576345f63a362d46
Gio4E20 <37852106+Gio4E20@users.noreply.github.com>
2020-12-16 01:57:23 -0300

    Add token parser

--- src/main/java/FreelanceAssister.java
+++ src/main/java/FreelanceAssister.java
@@ -1,14 +1,29 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
-import javax.security.auth.login.LoginException;
+import java.io.FileReader;
 
 public class FreelanceAssister {
 
     public static void main(String args[]) throws Exception {
 
-        JDA jda = new JDABuilder("").build();
+         JDA jda = new JDABuilder(getToken()).build();
 
     }
 
+    private static String getToken(){
+        JSONParser parser = new JSONParser();
+        try {
+            Object o = parser.parse(new FileReader(System.getProperty("user.dir") + "/src/main/resources/token.json"));
+            JSONObject object = (JSONObject) o;
+
+            return String.valueOf(object.get("token"));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
